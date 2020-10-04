36e382e67e3dde4575d7cf80ba961c2c81f70a1b
Lakshmi_N <64766689+stepin105082@users.noreply.github.com>
2020-10-04 22:41:33 +0530

    Add files via upload

--- MiniProject_C/3_Implementation/test/test_manage.c
+++ MiniProject_C/3_Implementation/test/test_manage.c
@@ -1,56 +1,72 @@
+#include "CUnit/Basic.h"
+#include <CUnit/CUnit.h>
+
+/* Modify these two lines according to the project */
+#include <manage.h>
+#define PROJECT_NAME    "Customer_Management"
 
 
-#include "unity.h"
-#include "manage.h"
-/* Required by the unity test framework */
-void setUp(){}
-/* Required by the unity test framework */
-void tearDown(){}
+/* Prototypes for all the test functions */
+void test_phone(void);
+void test_temperature_1(void);
+void test_temperature_2(void);
+void test_sanitize_check(void);
 
-/*Required for the first test cases*/
+/* Start of the application test */
+int main() {
+/* Note: Do not edit START*/
+  /*Initialize and setup the Test Framework */
+  if (CUE_SUCCESS != CU_initialize_registry())
+    return CU_get_error();
+  CU_pSuite suite = CU_add_suite(PROJECT_NAME, 0, 0);
+/* Note: Do not edit END */
 
+
+  /* Add your test functions in this format for testing*/
+  CU_add_test(suite, "phone", test_phone());
+  CU_add_test(suite, "temperature", test_temperature_1());
+  CU_add_test(suite, "temperature", test_temperature_2());
+  CU_add_test(suite, "sanitize", test_sanitize_check());
+
+
+/* Note: Do not edit START*/
+  /* Setup Test Framework to output the result to Screen */
+  CU_basic_set_mode(CU_BRM_VERBOSE);
+
+  /* run the unit test framework*/
+  CU_basic_run_tests();
+
+  /* Cleaning the Resources used by Unit test framework */
+  CU_cleanup_registry();
+/* Note: Do not edit END */
+  return 0;
+}
 
 struct Customer c={.phone="1234567890"};
 
 struct Customer d={.phone="123456"};
 
-void test_phone(void)
-{
-  TEST_ASSERT_EQUAL(1, phone_check(c));
-  TEST_ASSERT_EQUAL(0, phone_check(d));
+/* Write all the test functions */
+void test_phone(void) {
+  CU_ASSERT(1 == phone_check(c));
+  CU_ASSERT(0 == phone_check(d));
+}
+/*
+void test_subtract(void) {
+  CU_ASSERT(-3 == subtract(0, 3));
+
+  CU_ASSERT(1 == subtract(1000, 900));
 }
 
-void temperature_1(void)
-{
-  TEST_ASSERT_EQUAL(1, temp_check());
-  TEST_ASSERT_EQUAL(1, temp_check());
+void test_multiply(void) {
+  CU_ASSERT(0 == multiply(1, 0));
+
+  CU_ASSERT(2 == multiply(2, 5));
 }
 
-void sanitize_check(void)
-{
-  TEST_ASSERT_EQUAL(1, sanitize());
-  TEST_ASSERT_EQUAL(1, sanitize());
+void test_divide(void) {
+  CU_ASSERT(0 == divide(1, 0));
+
+  CU_ASSERT(3 == divide(2, 2));
 }
-
-void temperature_2(void)
-{
-    TEST_ASSERT_NOT_EQUAL(0,temp_check());
-    TEST_ASSERT_NOT_EQUAL(0,temp_check());
-}
-
-int test_main(void)
-{
-/* Initiate the Unity Test Framework */
-  UNITY_BEGIN();
-
-/* Run Test functions */
-  RUN_TEST(test_phone);
-  RUN_TEST(sanitize_check);
-  RUN_TEST(temperature_1);
-  RUN_TEST(temperature_2);
-
-
-  /* Close the Unity Test Framework */
-  return UNITY_END();
-}
-
+*/
